adb48dda96b7b660213bc5cc46a100d876a66823
Jesus Torres <j.torres3.dev@gmail.com>
2025-01-14 00:34:13 -0600

    metadata file fixed

--- app/lib/metadata.ts
+++ app/lib/metadata.ts
@@ -7,49 +7,29 @@ export const metadata = {
     "abstract": {
       "bg.png": {
         "name": "bg.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/backgrounds/abstract/bg.png",
-=======
-        "path": "/assets/backgrounds/abstract/bg.png",
->>>>>>> main
         "type": "png"
       },
       "greek-bi-view.png": {
         "name": "greek-bi-view.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/backgrounds/abstract/greek-bi-view.png",
-=======
-        "path": "/assets/backgrounds/abstract/greek-bi-view.png",
->>>>>>> main
         "type": "png"
       },
       "mex-wall-art.png": {
         "name": "mex-wall-art.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/backgrounds/abstract/mex-wall-art.png",
-=======
-        "path": "/assets/backgrounds/abstract/mex-wall-art.png",
->>>>>>> main
         "type": "png"
       }
     },
     "ui": {
       "footer-grid.svg": {
         "name": "footer-grid.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/backgrounds/ui/footer-grid.svg",
-=======
-        "path": "/assets/backgrounds/ui/footer-grid.svg",
->>>>>>> main
         "type": "svg"
       },
       "grid.svg": {
         "name": "grid.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/backgrounds/ui/grid.svg",
-=======
-        "path": "/assets/backgrounds/ui/grid.svg",
->>>>>>> main
         "type": "svg"
       }
     }
@@ -59,56 +39,32 @@ export const metadata = {
     "projects": {
       "b1.svg": {
         "name": "b1.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/covers/projects/b1.svg",
-=======
-        "path": "/assets/covers/projects/b1.svg",
->>>>>>> main
         "type": "svg"
       },
       "buff-hotel-system.png": {
         "name": "buff-hotel-system.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/covers/projects/buff-hotel-system.png",
-=======
-        "path": "/assets/covers/projects/buff-hotel-system.png",
->>>>>>> main
         "type": "png"
       },
       "instadvdiv.png": {
         "name": "instadvdiv.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/covers/projects/instadvdiv.png",
-=======
-        "path": "/assets/covers/projects/instadvdiv.png",
->>>>>>> main
         "type": "png"
       },
       "phaser3game.png": {
         "name": "phaser3game.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/covers/projects/phaser3game.png",
-=======
-        "path": "/assets/covers/projects/phaser3game.png",
->>>>>>> main
         "type": "png"
       },
       "rock-paper-scissors.png": {
         "name": "rock-paper-scissors.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/covers/projects/rock-paper-scissors.png",
-=======
-        "path": "/assets/covers/projects/rock-paper-scissors.png",
->>>>>>> main
         "type": "png"
       },
       "sound-wave-analysis-project.png": {
         "name": "sound-wave-analysis-project.png",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/covers/projects/sound-wave-analysis-project.png",
-=======
-        "path": "/assets/covers/projects/sound-wave-analysis-project.png",
->>>>>>> main
         "type": "png"
       }
     },
@@ -118,11 +74,7 @@ export const metadata = {
     "pdf": {
       "Resume Jesus Torres Software Engineer.pdf": {
         "name": "Resume Jesus Torres Software Engineer.pdf",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/docs/pdf/Resume Jesus Torres Software Engineer.pdf",
-=======
-        "path": "/assets/docs/pdf/Resume Jesus Torres Software Engineer.pdf",
->>>>>>> main
         "type": "pdf"
       }
     },
@@ -132,11 +84,7 @@ export const metadata = {
     "alerts": {
       "confetti.gif": {
         "name": "confetti.gif",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/gif/alerts/confetti.gif",
-=======
-        "path": "/assets/gif/alerts/confetti.gif",
->>>>>>> main
         "type": "gif"
       }
     }
@@ -145,568 +93,320 @@ export const metadata = {
     "dev": {
       "c.svg": {
         "name": "c.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/c.svg",
-=======
-        "path": "/assets/icons/dev/c.svg",
->>>>>>> main
         "type": "svg"
       },
       "dev-1.svg": {
         "name": "dev-1.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/dev-1.svg",
-=======
-        "path": "/assets/icons/dev/dev-1.svg",
->>>>>>> main
         "type": "svg"
       },
       "dev-lg.svg": {
         "name": "dev-lg.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/dev-lg.svg",
-=======
-        "path": "/assets/icons/dev/dev-lg.svg",
->>>>>>> main
         "type": "svg"
       },
       "dev-md.svg": {
         "name": "dev-md.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/dev-md.svg",
-=======
-        "path": "/assets/icons/dev/dev-md.svg",
->>>>>>> main
         "type": "svg"
       },
       "exp1.svg": {
         "name": "exp1.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/exp1.svg",
-=======
-        "path": "/assets/icons/dev/exp1.svg",
->>>>>>> main
         "type": "svg"
       },
       "exp2.svg": {
         "name": "exp2.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/exp2.svg",
-=======
-        "path": "/assets/icons/dev/exp2.svg",
->>>>>>> main
         "type": "svg"
       },
       "exp4.svg": {
         "name": "exp4.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/dev/exp4.svg",
-=======
-        "path": "/assets/icons/dev/exp4.svg",
->>>>>>> main
         "type": "svg"
       }
     },
     "social": {
       "advdiv.svg": {
         "name": "advdiv.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/advdiv.svg",
-=======
-        "path": "/assets/icons/social/advdiv.svg",
->>>>>>> main
         "type": "svg"
       },
       "github.svg": {
         "name": "github.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/github.svg",
-=======
-        "path": "/assets/icons/social/github.svg",
->>>>>>> main
         "type": "svg"
       },
       "gsap.svg": {
         "name": "gsap.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/gsap.svg",
-=======
-        "path": "/assets/icons/social/gsap.svg",
->>>>>>> main
         "type": "svg"
       },
       "host.svg": {
         "name": "host.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/host.svg",
-=======
-        "path": "/assets/icons/social/host.svg",
->>>>>>> main
         "type": "svg"
       },
       "insta.svg": {
         "name": "insta.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/insta.svg",
-=======
-        "path": "/assets/icons/social/insta.svg",
->>>>>>> main
         "type": "svg"
       },
       "linkedIn.svg": {
         "name": "linkedIn.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/linkedIn.svg",
-=======
-        "path": "/assets/icons/social/linkedIn.svg",
->>>>>>> main
         "type": "svg"
       },
       "twit.svg": {
         "name": "twit.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/twit.svg",
-=======
-        "path": "/assets/icons/social/twit.svg",
->>>>>>> main
         "type": "svg"
       },
       "wha.svg": {
         "name": "wha.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/social/wha.svg",
-=======
-        "path": "/assets/icons/social/wha.svg",
->>>>>>> main
         "type": "svg"
       }
     },
     "tech": {
       "bash.svg": {
         "name": "bash.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/bash.svg",
-=======
-        "path": "/assets/icons/tech/bash.svg",
->>>>>>> main
         "type": "svg"
       },
       "bootstrap.svg": {
         "name": "bootstrap.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/bootstrap.svg",
-=======
-        "path": "/assets/icons/tech/bootstrap.svg",
->>>>>>> main
         "type": "svg"
       },
       "csharp.svg": {
         "name": "csharp.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/csharp.svg",
-=======
-        "path": "/assets/icons/tech/csharp.svg",
->>>>>>> main
         "type": "svg"
       },
       "css.svg": {
         "name": "css.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/css.svg",
-=======
-        "path": "/assets/icons/tech/css.svg",
->>>>>>> main
         "type": "svg"
       },
       "dock.svg": {
         "name": "dock.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/dock.svg",
-=======
-        "path": "/assets/icons/tech/dock.svg",
->>>>>>> main
         "type": "svg"
       },
       "dotnet-v1.svg": {
         "name": "dotnet-v1.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/dotnet-v1.svg",
-=======
-        "path": "/assets/icons/tech/dotnet-v1.svg",
->>>>>>> main
         "type": "svg"
       },
       "dotnet.svg": {
         "name": "dotnet.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/dotnet.svg",
-=======
-        "path": "/assets/icons/tech/dotnet.svg",
->>>>>>> main
         "type": "svg"
       },
       "fm.svg": {
         "name": "fm.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/fm.svg",
-=======
-        "path": "/assets/icons/tech/fm.svg",
->>>>>>> main
         "type": "svg"
       },
       "git.svg": {
         "name": "git.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/git.svg",
-=======
-        "path": "/assets/icons/tech/git.svg",
->>>>>>> main
         "type": "svg"
       },
       "gitbash.svg": {
         "name": "gitbash.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/gitbash.svg",
-=======
-        "path": "/assets/icons/tech/gitbash.svg",
->>>>>>> main
         "type": "svg"
       },
       "html.svg": {
         "name": "html.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/html.svg",
-=======
-        "path": "/assets/icons/tech/html.svg",
->>>>>>> main
         "type": "svg"
       },
       "js.svg": {
         "name": "js.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/js.svg",
-=======
-        "path": "/assets/icons/tech/js.svg",
->>>>>>> main
         "type": "svg"
       },
       "jupyter.svg": {
         "name": "jupyter.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/jupyter.svg",
-=======
-        "path": "/assets/icons/tech/jupyter.svg",
->>>>>>> main
         "type": "svg"
       },
       "matplotlib.svg": {
         "name": "matplotlib.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/matplotlib.svg",
-=======
-        "path": "/assets/icons/tech/matplotlib.svg",
->>>>>>> main
         "type": "svg"
       },
       "mysql.svg": {
         "name": "mysql.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/mysql.svg",
-=======
-        "path": "/assets/icons/tech/mysql.svg",
->>>>>>> main
         "type": "svg"
       },
       "next.svg": {
         "name": "next.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/next.svg",
-=======
-        "path": "/assets/icons/tech/next.svg",
->>>>>>> main
         "type": "svg"
       },
       "nodejs.svg": {
         "name": "nodejs.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/nodejs.svg",
-=======
-        "path": "/assets/icons/tech/nodejs.svg",
->>>>>>> main
         "type": "svg"
       },
       "npm.svg": {
         "name": "npm.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/npm.svg",
-=======
-        "path": "/assets/icons/tech/npm.svg",
->>>>>>> main
         "type": "svg"
       },
       "nuget.svg": {
         "name": "nuget.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/nuget.svg",
-=======
-        "path": "/assets/icons/tech/nuget.svg",
->>>>>>> main
         "type": "svg"
       },
       "numpy.svg": {
         "name": "numpy.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/numpy.svg",
-=======
-        "path": "/assets/icons/tech/numpy.svg",
->>>>>>> main
         "type": "svg"
       },
       "phaser-planet-web.svg": {
         "name": "phaser-planet-web.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/phaser-planet-web.svg",
-=======
-        "path": "/assets/icons/tech/phaser-planet-web.svg",
->>>>>>> main
         "type": "svg"
       },
       "postman.svg": {
         "name": "postman.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/postman.svg",
-=======
-        "path": "/assets/icons/tech/postman.svg",
->>>>>>> main
         "type": "svg"
       },
       "py.svg": {
         "name": "py.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/py.svg",
-=======
-        "path": "/assets/icons/tech/py.svg",
->>>>>>> main
         "type": "svg"
       },
       "react.svg": {
         "name": "react.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/react.svg",
-=======
-        "path": "/assets/icons/tech/react.svg",
->>>>>>> main
         "type": "svg"
       },
       "s.svg": {
         "name": "s.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/s.svg",
-=======
-        "path": "/assets/icons/tech/s.svg",
->>>>>>> main
         "type": "svg"
       },
       "sql.svg": {
         "name": "sql.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/sql.svg",
-=======
-        "path": "/assets/icons/tech/sql.svg",
->>>>>>> main
         "type": "svg"
       },
       "ssms.svg": {
         "name": "ssms.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/ssms.svg",
-=======
-        "path": "/assets/icons/tech/ssms.svg",
->>>>>>> main
         "type": "svg"
       },
       "stream.svg": {
         "name": "stream.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/stream.svg",
-=======
-        "path": "/assets/icons/tech/stream.svg",
->>>>>>> main
         "type": "svg"
       },
       "streamName.svg": {
         "name": "streamName.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/streamName.svg",
-=======
-        "path": "/assets/icons/tech/streamName.svg",
->>>>>>> main
         "type": "svg"
       },
       "tail.svg": {
         "name": "tail.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/tail.svg",
-=======
-        "path": "/assets/icons/tech/tail.svg",
->>>>>>> main
         "type": "svg"
       },
       "three.svg": {
         "name": "three.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/three.svg",
-=======
-        "path": "/assets/icons/tech/three.svg",
->>>>>>> main
         "type": "svg"
       },
       "ts.svg": {
         "name": "ts.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/ts.svg",
-=======
-        "path": "/assets/icons/tech/ts.svg",
->>>>>>> main
         "type": "svg"
       },
       "vs.svg": {
         "name": "vs.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/vs.svg",
-=======
-        "path": "/assets/icons/tech/vs.svg",
->>>>>>> main
         "type": "svg"
       },
       "vscode.svg": {
         "name": "vscode.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/vscode.svg",
-=======
-        "path": "/assets/icons/tech/vscode.svg",
->>>>>>> main
         "type": "svg"
       },
       "wakatime.svg": {
         "name": "wakatime.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/wakatime.svg",
-=======
-        "path": "/assets/icons/tech/wakatime.svg",
->>>>>>> main
         "type": "svg"
       },
       "yarn.svg": {
         "name": "yarn.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/tech/yarn.svg",
-=======
-        "path": "/assets/icons/tech/yarn.svg",
->>>>>>> main
         "type": "svg"
       }
     },
     "text": {
       "appName.svg": {
         "name": "appName.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/text/appName.svg",
-=======
-        "path": "/assets/icons/text/appName.svg",
->>>>>>> main
         "type": "svg"
       },
       "cloudName.svg": {
         "name": "cloudName.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/text/cloudName.svg",
-=======
-        "path": "/assets/icons/text/cloudName.svg",
->>>>>>> main
         "type": "svg"
       },
       "dockerName.svg": {
         "name": "dockerName.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/text/dockerName.svg",
-=======
-        "path": "/assets/icons/text/dockerName.svg",
->>>>>>> main
         "type": "svg"
       },
       "hostName.svg": {
         "name": "hostName.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/text/hostName.svg",
-=======
-        "path": "/assets/icons/text/hostName.svg",
->>>>>>> main
         "type": "svg"
       }
     },
     "ui": {
       "app.svg": {
         "name": "app.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/app.svg",
-=======
-        "path": "/assets/icons/ui/app.svg",
->>>>>>> main
         "type": "svg"
       },
       "arrow.svg": {
         "name": "arrow.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/arrow.svg",
-=======
-        "path": "/assets/icons/ui/arrow.svg",
->>>>>>> main
         "type": "svg"
       },
       "b4.svg": {
         "name": "b4.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/b4.svg",
-=======
-        "path": "/assets/icons/ui/b4.svg",
->>>>>>> main
         "type": "svg"
       },
       "b5.svg": {
         "name": "b5.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/b5.svg",
-=======
-        "path": "/assets/icons/ui/b5.svg",
->>>>>>> main
         "type": "svg"
       },
       "cloud.svg": {
         "name": "cloud.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/cloud.svg",
-=======
-        "path": "/assets/icons/ui/cloud.svg",
->>>>>>> main
         "type": "svg"
       },
       "exp3.svg": {
         "name": "exp3.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/exp3.svg",
-=======
-        "path": "/assets/icons/ui/exp3.svg",
->>>>>>> main
         "type": "svg"
       },
       "pdf.svg": {
         "name": "pdf.svg",
-<<<<<<< HEAD
         "path": "/Nextjs-Asset-Host/assets/icons/ui/pdf.svg",
-=======
-        "path": "/assets/icons/ui/pdf.svg",
->>>>>>> main
         "type": "svg"
       }
     }
